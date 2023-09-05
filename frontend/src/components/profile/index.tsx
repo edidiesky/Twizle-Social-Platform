267b3f9c95c8d41b3e2ace1145ee98c94a8f34ec
edidiesky <essienedidiong1000@gmail.com>
2023-09-04 17:45:21 -0700

    new fixes

--- frontend/src/components/profile/index.tsx
+++ frontend/src/components/profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import WallpaperIndex from './top/wallpaper';
 import ProfileBottomIndex from './top/bottom';
@@ -18,10 +18,12 @@ type Rightbar = {
 
 const Profile: React.FC = () => {
     const { name } = useParams()
+    const [tab, setTab] = useState(0)
+    const feed = false
     // console.log(name)
     const [modal, setModal] = React.useState(false)
 
-    const { loginisLoading, loginisSuccess } = useAppSelector(store => store.auth)
+    const { userDetails } = useAppSelector(store => store.auth)
 
     const dispatch = useAppDispatch()
 
@@ -50,12 +52,29 @@ const Profile: React.FC = () => {
                 </div>
                 <div className="w-100 flex column">
                     <div className="w-100 flex item-center text-bold fs-16 profilelist">
-                        <div className="flex-1 active profileTag"><div className="tag">Tweets</div></div>
-                        <div className="flex-1 profileTag">Replies</div>
-                        <div className="flex-1 profileTag">Media</div>
-                        <div className="flex-1 profileTag">Likes</div>
+                        <div className="flex-1 profileTag">
+                            <div onClick={() => setTab(0)} className={tab === 0 ? "tag active" : "tag text-light"}>Tweets</div>
+                        </div>
+                        <div className="flex-1 profileTag">
+                            <div onClick={() => setTab(1)} className={tab === 1 ? "tag active" : "tag text-light"}>Replies</div>
+                        </div>
+                        <div className="flex-1 profileTag">
+                            <div onClick={() => setTab(2)} className={tab === 2 ? "tag active" : "tag text-light"}>Media</div>
+                        </div>
+                        <div className="flex-1 profileTag">
+                            <div onClick={() => setTab(4)} className={tab === 4 ? "tag active" : "tag text-light"}>Likes</div>
+                        </div>
                     </div>
-                    <Feed />
+                    {
+                        !feed ? <div className="flex w-85 auto py-2 item-center justify-center">
+                            <h2 style={{lineHeight:"1.3", width:"60%"}} className="fs-30 w-85 auto text-bold">
+                                @{userDetails?.display_name} hasn’t posted
+
+                                <span className="text-light fs-14 block text-grey">When they do, their posts will show up here.</span>
+                            </h2>
+                        </div> : <Feed />
+                    }
+
                 </div>
             </div>
             <RightSidebarIndex types={'profile'} />
@@ -71,15 +90,40 @@ const ProfileStyles = styled.div`
     height: 100vh;
     overflow: auto;
     align-items: flex-start;
+  border-right: 1px solid var(--border);
+  border-left: 1px solid var(--border);
+
     @media (min-width:1380px) {
         max-width: 1380px;
         margin: 0 auto;
     }
+     .tag {
+        cursor: pointer;
+            width: max-content;
+            margin: 0 auto;
+            /* background-color: red; */
+            position: relative;
+            &.active {
+            position: relative;
+             &::after {
+                position: absolute;
+                width: 100%;
+                content: '';
+                left: 0;
+                background-color: var(--blue-1);
+                height: 4px;
+                border-radius: 10px;
+                bottom: -100%;
+            }
+            }
+           
+        }
     .profilelist {
         border-bottom:1px solid rgba(0,0,0,.1);
         .profileTag {
-            padding:2rem;
+            padding:1.7rem 2rem;
             text-align:center;
+            
             &:hover {
                 background:var(--grey-2);
             }
