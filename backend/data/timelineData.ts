bb26c2c8afbe05291737082e322d8ebc0598035c
edidiesky <essienedidiong1000@gmail.com>
2023-12-11 07:02:49 -0800

    new fixes

--- backend/data/timelineData.ts
+++ backend/data/timelineData.ts
@@ -1,100 +1,155 @@
 export const timelineData = [
-    // .Center Devils 
-   
-   
-    // 650e40bc92f7f0389b9040dc
-    {
-        tweet_text: "PSA for everyone who leaves their phones on silent and is about to get an iPhone 15 Pro: You can turn off the annoying permanent silent indicator in settings. You’re welcome",
-        tweet_image: ['https://pbs.twimg.com/media/F6o6Q-2XIAAItiP?format=jpg&name=medium','https://pbs.twimg.com/media/F6o6Q-4XcAA0Jjr?format=jpg&name=medium'],
-        tweet_user_id: "650e40bc92f7f0389b9040dc"
-    }, 
+    // .Center Devils
 
-    // 650e40bc92f7f0389b9040dd
+ 
+
+    // COD
+    // 65771a970d6369351e07f535
     {
-        tweet_text: "Xavi on Barça decisions for João Félix, and Cancelo loans: “Now there are nine months left and that is a long time”. 🔵🔴🇵🇹 #FCB“ We will have to define and talk to their clubs, but right now it is unthinkable to discuss about it”.",
-        tweet_image: ['https://pbs.twimg.com/media/F6oQNubWYAEmBmi?format=jpg&name=large'],
-        tweet_user_id: "650e40bc92f7f0389b9040dd"
+        tweet_text: "Thank You @CallofDuty for the delivery 🍕 #CallofDuty #Sweepstakes",
+        tweet_image: [
+            'https://pbs.twimg.com/media/GAsNarmXcAAE1Un?format=jpg&name=small'
+        ],
+        tweet_user_id: "65771a970d6369351e07f535"
     },
     // 650e40bc92f7f0389b9040dd
     {
         tweet_text: "🚨🔴⚪️ Official: Martin Ødegaard has signed new long term deal at Arsenal — valid until June 2028.“Signing a new contract was a really easy decision for me, what we are doing right now as a club is special, and I want to be a part of that”, Ødegaard says.Never in doubt 🔒✨",
         tweet_image: ['https://pbs.twimg.com/media/F6nYISOXAAATvly?format=jpg&name=900x900'],
-        tweet_user_id: "650e40bc92f7f0389b9040dd"
+        tweet_user_id: "65771a970d6369351e07f53a"
     }, 
-    {
-        tweet_text: `Xavi: "My 𝐛𝐞𝐬𝐭 moment as a Barça coach? The Super Cup Final against Real Madrid." #fcblive 💪`,
-        tweet_image: ['https://pbs.twimg.com/media/F6oKLfvWIAAybPJ?format=jpg&name=small'],
-        tweet_user_id: "650e40bc92f7f0389b9040dc"
-    }, 
-    // Barca times
-    {
-        tweet_text: "📅| Barcelona's updated game schedule for September. #fcblive 🔵🔴",
-        tweet_image: ['https://pbs.twimg.com/media/F5nalslWMAA6Ek0?format=jpg&name=small'],
-        tweet_user_id: "650e40bc92f7f0389b9040de"
-    },
-    // Barca Times
-     {
-        tweet_text: "🚨 | BREAKING: According to @ESPNBrasil Roque's injury could be more serious. The Brazilian's ligaments could potentially be affected.Recovery will take more than three months.#fcblive",
-         tweet_image: ['https://pbs.twimg.com/media/F6pYh1qXUAI2Zv9?format=jpg&name=small'],
-        tweet_user_id: "650e40bc92f7f0389b9040de"
-    },
-    {
-        tweet_text: "🚨🚨🎙️| Erik ten Hag on Casemiro: `He’s the one.I’ve just mentioned, you have to stay in the game.You have to show the leadership in those moments when the team have difficult moments, difficult occasions, and we have to survive. All the players can be with him.He shows the example that all the players follow.That’s Casemiro.In the end, he scored two goals.`",
-        tweet_image: ['https://pbs.twimg.com/media/F6jjGpDbMAAXF95?format=jpg&name=medium'],
-        tweet_user_id: "650e40bc92f7f0389b9040d8"
-    },
-    {
-        tweet_text: "One of these was shot with iPhone 15 Pro, and the other was shot with Pixel Fold 🔎",
-        tweet_image: ['https://pbs.twimg.com/media/F6jsBllXAAAz8TY?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/F6jsBllXcAAb_iL?format=jpg&name=4096x4096'],
-        tweet_user_id: "650e40bc92f7f0389b9040dc"
-    },
     // food
     {
         tweet_text: "How do you like your noodles to be prepared?😋",
         tweet_image: ['https://pbs.twimg.com/media/F5uioLVWIAAzoL1?format=jpg&name=medium', 'https://pbs.twimg.com/media/F5uioLNWYAAac4I?format=jpg&name=medium',
             'https://pbs.twimg.com/media/F5uioLPWIAEzA5d?format=jpg&name=medium', 'https://pbs.twimg.com/media/F5uioLIXMAAthC2?format=jpg&name=medium'
         ],
-        tweet_user_id: "650e40bc92f7f0389b9040da"
+        tweet_user_id: "65771a970d6369351e07f537"
+    },
+
+    // Lebron
+    // 65771a970d6369351e07f533
+
+    {
+        tweet_text: "The LeBron James Family Foundation announces the  first official @KingJamesmuseum, set to open November 25th in Akron, Ohio.The museum will have never - before - seen - items from his career and proceeds will go to back into the community.",
+        tweet_image: [
+            'https://pbs.twimg.com/media/F-fgj7FWQAA_0Rk?format=jpg&name=small',
+        ],
+        tweet_user_id: "65771a970d6369351e07f533"
+    },
+    // Barca times
+    {
+        tweet_text: "📅| Barcelona's updated game schedule for September. #fcblive 🔵🔴",
+        tweet_image: ['https://pbs.twimg.com/media/F5nalslWMAA6Ek0?format=jpg&name=small'],
+        tweet_user_id: "65771a970d6369351e07f53b"
     },
     {
         tweet_text: "Life is a series of development of which begining we can ascertain but of which end is an endless journey of great and timeless oppportunity!",
         tweet_image: [],
-        tweet_user_id: "650e40bc92f7f0389b9040da"
+        tweet_user_id: "65771a970d6369351e07f537"
     },
     // .Center Devils 
     {
         tweet_text: "Who'll win the first Women's Nations League ⁉️🤔",
         tweet_image: ['https://pbs.twimg.com/media/F6nYzr3W4AAI2Yv?format=jpg&name=large'],
-        tweet_user_id: "650e40bc92f7f0389b9040d8"
-    },
-    // 650e40bc92f7f0389b9040dc
-
-    // .Center Devils 
-    {
-        tweet_text: "🚨 Manchester United are prepared to offload Jadon Sancho in January and are willing to accept a significant financial loss on the £73M they paid for him in 2021.",
-        tweet_image: ['https://pbs.twimg.com/media/F6k3qT2WEAAT7Az?format=jpg&name=900x900'],
-        tweet_user_id: "650e40bc92f7f0389b9040d8"
+        tweet_user_id: "65771a970d6369351e07f536"
     },
     // Barca Times
     {
         tweet_text: "🚨 | BREAKING: According to @ESPNBrasil Roque's injury could be more serious. The Brazilian's ligaments could potentially be affected.Recovery will take more than three months.#fcblive",
         tweet_image: ['https://pbs.twimg.com/media/F6pYh1qXUAI2Zv9?format=jpg&name=small'],
-        tweet_user_id: "650e40bc92f7f0389b9040de"
-    },
-    // 650e40bc92f7f0389b9040db
-    {
-        tweet_text: "I remember one year, I applied for a job. My friend sent me the link, he said he had just applied and suggested I give it a try too. He said they are a multinational company and trying to expand to Nigeria. Lol, I quickly applied too. I needed the job so bad",
-        tweet_image: [''],
-        tweet_user_id: "650e40bc92f7f0389b9040db"
-    },
-    // .Center Devils
-    {
-        tweet_text: "As I leave this club after many years of incredible memories, outstanding teammates, and trophies, I want to express my profound gratitude. This chapter may be closing, but the friendships and memories will forever remain in my heart. The fans here are the real champions. Thanks… Show more",
-        tweet_image: ['https://pbs.twimg.com/media/F5W8W1kWEAAZbdU?format=jpg&name=small'],
-        tweet_user_id: "650e40bc92f7f0389b9040d8"
+        tweet_user_id: "65771a970d6369351e07f53a"
     },
 
+      // Mr Beast
+    // 65771a970d6369351e07f534
+    {
+        tweet_text: "The GTA 6 trailer just broke our record for most views on YouTube in 24 hours 😮",
+        tweet_image: [
+            'https://pbs.twimg.com/media/GAmDKcMXsAEdZSc?format=jpg&name=small'
+        ],
+        tweet_user_id: "65771a970d6369351e07f534"
+    },
+    // 65771a970d6369351e07f539
+    {
+        tweet_text: "PSA for everyone who leaves their phones on silent and is about to get an iPhone 15 Pro: You can turn off the annoying permanent silent indicator in settings. You’re welcome",
+        tweet_image: ['https://pbs.twimg.com/media/F6o6Q-2XIAAItiP?format=jpg&name=medium', 'https://pbs.twimg.com/media/F6o6Q-4XcAA0Jjr?format=jpg&name=medium'],
+        tweet_user_id: "65771a970d6369351e07f539"
+    },
+
+
+    // COD
+    // 65771a970d6369351e07f535
+    {
+        tweet_text: "You'll wonder how you lived without them #ModernWarfareZombies 🧟",
+        tweet_image: [
+            'https://pbs.twimg.com/media/GA2JJ2aWUAAmZTr?format=jpg&name=360x360',
+            'https://pbs.twimg.com/media/GA2JK9eWYAAvvEc?format=jpg&name=360x360',
+            'https://pbs.twimg.com/media/GA2JMF-XgAA8QiO?format=jpg&name=360x360',
+            '',
+        ],
+        tweet_user_id: "65771a970d6369351e07f535"
+    },
+
+    //
+    // Lebron
+    // 65771a970d6369351e07f533
+
+    {
+        tweet_text: "So INSANE!!!! 😬😬😬😬😬",
+        tweet_image: [
+            'https://pbs.twimg.com/media/F_oQRCeXwAAaVGs?format=jpg&name=360x360',
+            'https://pbs.twimg.com/media/F_oQQ0BWwAAeP0A?format=jpg&name=360x360',
+            'https://pbs.twimg.com/media/F_oQRRzXMAA7w_Y?format=jpg&name=360x360',
+        ],
+        tweet_user_id: "65771a970d6369351e07f533"
+    },
+    // Mr Beast
+    {
+        tweet_text: "I spent a week buried alive because you all really liked when I did it for 50 hours.. It was hard af, go watch!",
+        tweet_image: [
+            'https://pbs.twimg.com/media/F_O0-h6WsAAJg_E?format=jpg&name=small'
+        ],
+        tweet_user_id: "65771a970d6369351e07f534"
+    },
+
+   
+    //    
+    // Lebron
+
+    {
+        tweet_text: "Up watching this Barry Sanders documentary and I’m getting hella emotional. This guy is simply INCREDIBLE!!!! 💐 💐💐💐💐 @BarrySanders",
+        tweet_image: [],
+        tweet_user_id: "65771a970d6369351e07f533"
+    },
+
+   
+    {
+        tweet_text: "🚨 Manchester United are prepared to offload Jadon Sancho in January and are willing to accept a significant financial loss on the £73M they paid for him in 2021.",
+        tweet_image: ['https://pbs.twimg.com/media/F6k3qT2WEAAT7Az?format=jpg&name=900x900'],
+        tweet_user_id: "65771a970d6369351e07f536"
+    },
+    // COD
+    // 65771a970d6369351e07f535
+    {
+        tweet_text: "Tomorrow, meet Meat 🥩 Here is your full guide to the new fast paced 6v6 #MW3 Multiplayer map coming with Season 1 👉 https://a.atvi.com/MWIII-S1Meat",
+        tweet_image: [
+            'https://pbs.twimg.com/media/GAnxCh_W8AAYTQK?format=jpg&name=small'
+        ],
+        tweet_user_id: "65771a970d6369351e07f535"
+    },
+
+    // COD
+    // 65771a970d6369351e07f535
+    {
+        tweet_text: "Assemble the squad. Season 1 arrives December 6 in #MW3 and Call of Duty #Warzone 🔥  https://a.atvi.com/MWIII-S1Roadmap",
+        tweet_image: [
+            'https://pbs.twimg.com/media/GAMuGPJa8AEOzHQ?format=jpg&name=small'
+        ],
+        tweet_user_id: "65771a970d6369351e07f535"
+    },
 ];
+
 
 //  2. Itohan 3. babajide 4. Ericc Baily 5.Center Devils 6. Edo Boy
 // 650e40bc92f7f0389b9040d8
