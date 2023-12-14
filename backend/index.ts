1f233724fc543e8bb02178e3d99c482df41593cd
edidiesky <essienedidiong1000@gmail.com>
2023-12-13 18:32:21 -0800

    new

--- backend/index.ts
+++ backend/index.ts
@@ -105,24 +105,24 @@ const clienIdSecret: string = process.env.google_client_secret as string
 
 
 // routes
-import usertweetRoute from "./routes/userTweetRoute";
-import userRoute from "./routes/userRoute";
-import authRoute from "./routes/authRoute";
-import quoteRoute from "./routes/quoteTweetRoute";
-// import uploadRoute from "./routes/uploadRoute";
-import conversationRoute from "./routes/conversationRoute";
-import messageRoute from "./routes/messageRoute";
+// import usertweetRoute from "./routes/userTweetRoute";
+// import userRoute from "./routes/userRoute";
+// import authRoute from "./routes/authRoute";
+// import quoteRoute from "./routes/quoteTweetRoute";
+// // import uploadRoute from "./routes/uploadRoute";
+// import conversationRoute from "./routes/conversationRoute";
+// import messageRoute from "./routes/messageRoute";
 import commentRoute from "./routes/commentRoute";
 
 
 
-app.use("/api/v1/tweet", usertweetRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/auth", authRoute);
-// app.use("/api/v1/upload", uploadRoute);
-app.use("/api/v1/quote", quoteRoute);
-app.use("/api/v1/conversation", conversationRoute);
-app.use("/api/v1/message", messageRoute);
+// app.use("/api/v1/tweet", usertweetRoute);
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/auth", authRoute);
+// // app.use("/api/v1/upload", uploadRoute);
+// app.use("/api/v1/quote", quoteRoute);
+// app.use("/api/v1/conversation", conversationRoute);
+// app.use("/api/v1/message", messageRoute);
 app.use("/api/v1/comment", commentRoute);
 
 
