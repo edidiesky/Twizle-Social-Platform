80c9a9970ad4f21fe44132c4e595a2a62feb60cc
edidiesky <essienedidiong1000@gmail.com>
2023-12-14 15:31:16 -0800

    new fixes

--- backend/routes/uploadRoute.ts
+++ backend/routes/uploadRoute.ts
@@ -17,55 +17,55 @@ const router = express.Router();
 // Configure Multer
 const upload = multer({ dest: 'airbnb/' });
 
-router.post("/", upload.array("files", 4), async (req, res) => {
-  try {
-    // req.files contains an array of uploaded files
-    const files: Express.Multer.File[] = req.files as Express.Multer.File[];
+// router.post("/", upload.array("files", 4), async (req, res) => {
+//   try {
+//     // req.files contains an array of uploaded files
+//     const files: Express.Multer.File[] = req.files as Express.Multer.File[];
 
 
-    // Use a loop to upload each file to Cloudinary
-    const urls = [];
-    for (const file of files) {
-      const result = await cloudinaryModule.uploader.upload(file.path);
-      // Check if 'result' has a 'secure_url' property
-      if (result && result?.secure_url) {
-        // Explicitly cast 'result.secure_url' to string
-        const secureUrl: never = result?.secure_url as unknown as never;
-        urls.push(secureUrl);
-      } else {
-        // Handle the case where 'secure_url' is not present in the response
-        console.log('no url')
-      }
-    }
+//     // Use a loop to upload each file to Cloudinary
+//     const urls = [];
+//     for (const file of files) {
+//       const result = await cloudinaryModule.uploader.upload(file.path);
+//       // Check if 'result' has a 'secure_url' property
+//       if (result && result?.secure_url) {
+//         // Explicitly cast 'result.secure_url' to string
+//         const secureUrl: never = result?.secure_url as unknown as never;
+//         urls.push(secureUrl);
+//       } else {
+//         // Handle the case where 'secure_url' is not present in the response
+//         console.log('no url')
+//       }
+//     }
 
-    // Optionally, you can respond with the URLs of the uploaded images
-  res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, message: "Images uploaded successfully", urls });
-  } catch (error) {
-    console.error("Error uploading images:", error);
+//     // Optionally, you can respond with the URLs of the uploaded images
+//   res.setHeader('Content-Type', 'application/json');
+//     res.json({ success: true, message: "Images uploaded successfully", urls });
+//   } catch (error) {
+//     console.error("Error uploading images:", error);
 
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to upload images" });
-  }
-});
+//     res
+//       .status(500)
+//       .json({ success: false, message: "Failed to upload images" });
+//   }
+// });
 
-router.post("/single", upload.single("files"), async (req, res) => {
-  try {
-    // req.files contains an array of uploaded files
-    const files: Express.Multer.File = req.file as Express.Multer.File;
+// router.post("/single", upload.single("files"), async (req, res) => {
+//   try {
+//     // req.files contains an array of uploaded files
+//     const files: Express.Multer.File = req.file as Express.Multer.File;
 
-    const result = await cloudinaryModule.uploader.upload(files?.path);
-    // Optionally, you can respond with the URLs of the uploaded images
-  res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, message: "Images uploaded successfully", urls: result?.secure_url });
-  } catch (error) {
-    console.error("Error uploading images:", error);
+//     const result = await cloudinaryModule.uploader.upload(files?.path);
+//     // Optionally, you can respond with the URLs of the uploaded images
+//   res.setHeader('Content-Type', 'application/json');
+//     res.json({ success: true, message: "Images uploaded successfully", urls: result?.secure_url });
+//   } catch (error) {
+//     console.error("Error uploading images:", error);
 
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to upload images" });
-  }
-});
+//     res
+//       .status(500)
+//       .json({ success: false, message: "Failed to upload images" });
+//   }
+// });
 
 export default router 
