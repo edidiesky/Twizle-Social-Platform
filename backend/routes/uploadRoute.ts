66d2540e73b8aa1491a9f60d749ab9f3575b1bb2
edidiesky <essienedidiong1000@gmail.com>
2023-12-14 15:33:40 -0800

    new

--- backend/routes/uploadRoute.ts
+++ backend/routes/uploadRoute.ts
@@ -1,21 +1,21 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import cloudinary from "cloudinary";
-import multer from "multer";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import cloudinary from "cloudinary";
+// import multer from "multer";
 
-const cloudinaryModule = cloudinary.v2;
-// console.log(process.env.cloud_name);
+// const cloudinaryModule = cloudinary.v2;
+// // console.log(process.env.cloud_name);
 
-cloudinaryModule.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.cloud_key,
-  api_secret: process.env.cloud_secret,
-});
+// cloudinaryModule.config({
+//   cloud_name: process.env.cloud_name,
+//   api_key: process.env.cloud_key,
+//   api_secret: process.env.cloud_secret,
+// });
 
 const router = express.Router();
 // Configure Multer
-const upload = multer({ dest: 'airbnb/' });
+// const upload = multer({ dest: 'airbnb/' });
 
 // router.post("/", upload.array("files", 4), async (req, res) => {
 //   try {
